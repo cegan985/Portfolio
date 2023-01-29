301ed5c4295af45f03ce894d542ff947b255e10a
Connor Egan <connoregan@Connors-MacBook-Pro.local>
2023-01-29 10:07:47 -0600

    Update portfolio

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -5,12 +5,24 @@ type Props = {}
 
 export default function BackgroundCircles({}: Props) {
   return (
-    <div className='relative flex justify-center items-center'>
-        <div className='absolute border border-[rgb(139,148,158)] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
-        <div className='absolute border border-[rgb(139,148,158)] rounded-full h-[300px] w-[300px] mt-52'/>
-        <div className='absolute border border-[rgb(139,148,158)] rounded-full h-[500px] w-[500px] mt-52'/>
+    <motion.div 
+    initial={{
+        opacity: 0,
+    }}
+    animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        borderRadius: ['20%', '20%', '50%', '80%', '20%']
+    }}
+    transition={{
+        duration: 2.5,
+    }}
+    className='relative flex justify-center items-center'>
+        <div className='absolute border border-[rgb(71,44,82)] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
+        <div className='absolute border border-[rgb(52,57,65)] rounded-full h-[300px] w-[300px] mt-52  animate-pulse'/>
+        <div className='absolute border border-[rgb(139,148,158)] rounded-full h-[500px] w-[500px] mt-52  animate-pulse'/>
         <div className='absolute border border-[rgb(71,44,82)] opacity-20 rounded-full h-[200px] w-[200px] mt-52 animate-pulse'/>
-        <div className='absolute border border-[rgb(139,148,158)] rounded-full h-[800px] w-[800px] mt-52'/>
-    </div>
+        <div className='absolute border border-[rgb(52,57,65)] rounded-full h-[800px] w-[800px] mt-52  animate-pulse'/>
+    </motion.div>
   )
 }
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -2,22 +2,29 @@ import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles'
 
+
 type Props = {}
 
 export default function Profile({}: Props) {
     const [text, count] = useTypewriter({
-        words: ['Hi, my name is Connor Egan','Developer', 'Designer', 'Creator'],
+        words: ['Hi, my name is Connor Egan', 'I am a', 'Developer', 'Designer', 'Creator'],
         loop: true,
         delaySpeed: 2000
     })
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircles />
+        <img src='IMG_7766.JPG' alt=''/>
+        <div>
+            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+                Software Engineer
+            </h2>
         <h1>
         <span>{text}</span>
         <Cursor cursorColor='#472c52' />
 
         </h1>
+        </div>
     </div>
   )
 }
